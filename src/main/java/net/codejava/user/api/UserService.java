c07b833baaec003e0c7aad2a3dcb58ab8f3c2053
MuraliPanneerSelvam <pmuralivijay937@gmail.com>
2023-08-23 09:26:08 +0530

    Completed

---
+++ src/main/java/net/codejava/user/api/UserService.java
@@ -0,0 +1,29 @@
+package net.codejava.user.api;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import net.codejava.user.User;
+import net.codejava.user.UserRepository;
+
+@Service
+@Transactional
+public class UserService {
+	
+	@Autowired 
+	UserRepository repo;
+	
+	@Autowired 
+	PasswordEncoder passwordEncoder;
+	
+	public User save(User user) {
+		String rawPassword = user.getPassword();
+		String encodedPassword = passwordEncoder.encode(rawPassword);
+		user.setPassword(encodedPassword);
+		
+		return repo.save(user);
+	}
+}
